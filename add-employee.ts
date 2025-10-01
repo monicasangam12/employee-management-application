54246442975ff304dcf4d243a6026edf08e2e75b
monicasangam12 <47582209+monicasangam12@users.noreply.github.com>
2025-10-01 15:31:57 -0400

    Update add-employee.ts

--- add-employee.ts
+++ add-employee.ts
@@ -1,27 +1,76 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 import { Employee } from '../../employee';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpErrorResponse, HttpClientModule } from '@angular/common/http';
+import { EmployeeService } from '../employee-service';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, HttpClientModule],
+  standalone: true,
+  providers: [EmployeeService],
   templateUrl: './add-employee.html',
   styleUrl: './add-employee.css'
 })
-export class AddEmployee {
-    employeeForm!: FormGroup;
+export class AddEmployeeComponent implements OnInit {
+  @Input() id!: number;
+  employeeForm!: FormGroup;
 
-    constructor(private fb: FormBuilder){
-      this.employeeForm = this.fb.group({
-          username: ['', Validators.required],
-          password: ['', Validators.required]
-      });
-      console.log(this.employeeForm);
-    }
+  router: any;
 
-    addEmployee(employeeForm: FormGroup){
-       const employee = new Employee(1, "", "", 0, "", "");
-       console.log(employee);
-       console.log("Add Employer Clicked");
+  constructor(private fb: FormBuilder, private employeeService: EmployeeService){
+    this.employeeForm = this.fb.group({
+      name: [''],
+      username: [''],
+      password: [''],
+      position: [''],
+      salary: [''],
+      rating: ['']
+    });
+    console.log(this.employeeForm);
+    console.log ("Add Employee Component Loaded mode =", this.id);
+  }
+  ngOnInit() {
+    // The 'id' property is automatically set by the router
+    console.log('mode ID:', this.id);
+  }
+
+  addEmployee(employee: Employee){ 
+    //patch the  form values
+    this.employeeForm.patchValue({
+      name: employee.name,
+      username: employee.username,
+      password: employee.password,
+      position: employee.position,
+      salary: employee.salary,
+      rating: employee.rating
+    });
+    console.log(employee);
+    console.log("Add Employee Clicked");
+  }
+
+  onSubmit(){
+      if(this.employeeForm.valid){
+        console.log("Form Submitted Successfully value",this.employeeForm.value);
+        console.log("Form Submitted Successfully value",this.employeeForm.value.name);
+
+        this.employeeService.postEmployeeDetails(this.employeeForm.value).subscribe({
+          next: (response: Employee) => {
+            console.log(response);
+            console.log("Employee registered successfully", response);
+            this.router.navigate(['/employees']);
+          },
+          error: (error: HttpErrorResponse) => {
+            console.log(error);
+            console.error("Error during registration", error);
+          }
+        });
+      }
+      else{
+        console.log("Registration form is not valid");
+      }
+  
     }
 }
+
