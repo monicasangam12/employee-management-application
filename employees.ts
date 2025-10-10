f886c0c7fdd68e0379cbf493fba26ecd83363a8c
monicasangam12 <47582209+monicasangam12@users.noreply.github.com>
2025-10-10 13:41:15 -0400

    Update employees.ts to applyFilter method with Target Event client side not server side

--- employees.ts
+++ employees.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
-import { Employee } from '../../employee';
-import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, EventEmitter, ViewChild } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
-import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { EmployeeService } from '../employee-service';
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatButton } from "@angular/material/button";
 import { Router } from '@angular/router';
-import { AddEmployeeService } from '../addEmployee.service';
-// import { RegistrationService } from '../registration.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppFilterPipe } from '../../filter.pipe';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+
 
 const COLUMNS_SCHEMA = [
   {
@@ -33,6 +33,11 @@ const COLUMNS_SCHEMA = [
       label: "Name"
   },
   {
+      key: "email",
+      type: "text",
+      label: "Email"
+  },
+  {
     key: "position",
     type: "text",
     label: "Position"
@@ -41,6 +46,11 @@ const COLUMNS_SCHEMA = [
       key: "salary",
       type: "number",
       label: "Salary"
+  },
+  {
+      key: "rating",
+      type: "number",
+      label: "Rating"
   },
   {
     key: "username",
@@ -55,133 +65,79 @@ const COLUMNS_SCHEMA = [
 
 ];
 
+export enum FORM_MODE {
+      Register = 0,
+      Add = 1,
+      Edit = 2,
+      Delete = 3
+}
+
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule, MatTableModule, MatPaginatorModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, HttpClientModule, MatCheckboxModule, MatButton, MatSortModule],
-  providers: [EmployeeService, HttpClientModule],
+  imports: [CommonModule, MatInputModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButton, FormsModule, HttpClientModule, MatSortModule],
+  providers: [EmployeeService],
   templateUrl: './employees.html',
   styleUrl: './employees.css'
 })
 export class EmployeesComponent {
-    $newEmployee!: Employee;
-    employees: Employee[] = [];
-    columnsSchema: any = COLUMNS_SCHEMA;
-    displayedColumns: string[] = COLUMNS_SCHEMA.map((col: { key: any; }) => {   return col.key });
-    dataSource = new MatTableDataSource<Employee>(this.employees);
-    selection = new SelectionModel<Employee>(true, []);
-    @ViewChild(MatPaginator) paginator!: MatPaginator;
-    employeeForm: FormGroup;
-    //sorting data for table
-    @ViewChild(MatSort)sort: MatSort = new MatSort;
 
-    
-     constructor(private fb: FormBuilder, private employeeService: EmployeeService, private http: HttpClient, private router: Router)  {
-      this.employeeService.getData().subscribe((data: Employee[]) => {
-        this.employees = data;
-        let newEmployees: Employee[] = [];
-        
-        for (let emp of this.employees) {
-          let newEmp = {
-            id: emp.id,
-            name: emp.name,
-            position: emp.position,
-            salary: emp.salary,
-            username: emp.username,
-            password: emp.password,
-            rating: emp.rating,
-            isEdit: false
-          }
-          newEmployees.push(newEmp);
-        }
-        console.log("Employees Data from Service in Constructor:", newEmployees);
+  //Table Definition
+  columnsSchema: any = COLUMNS_SCHEMA;
+  displayedColumns: string[] = COLUMNS_SCHEMA.map((col: { key: any; }) => {   return col.key });
 
-        this.dataSource.data = newEmployees;
-        console.log("Fetched Employees Data:", this.dataSource.data);
-        this.dataSource.paginator = this.paginator; 
-      });
-        this.employeeForm = this.fb.group({
-            select: [ { checked: false }],
-            id: [this.dataSource.data.values().next().value?.id || 0],
-            name: [this.dataSource.data.values().next().value?.name || ''],
-            position: [this.dataSource.data.values().next().value?.position || ''],
-            salary: [this.dataSource.data.values().next().value?.salary || 0],
-            username: [this.dataSource.data.values().next().value?.username || ''],
-            password: [this.dataSource.data.values().next().value?.password || ''],
-            rating: [this.dataSource.data.values().next().value?.rating || 0],
-            isEdit: [false]
-        });
-        console.log(this.employeeForm);
-    }
+  //Data Source and paginator
+  dataSource = new MatTableDataSource<Employee>();  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-    ngOnInit() {
-        this.employeeService.getData().subscribe(data => {
-          this.employees = data;
-          console.log("Employees Data from Service:", this.employees);
-        });
+  //selection model for employee
+  selection = new SelectionModel<Employee>(true, []);
 
-        this.employeeForm = this.fb.group({
-          rows: this.fb.array([
-            {
-              select: [false],
-              name: [this.dataSource.data.values().next().value?.name || ''],
-              username: [this.dataSource.data.values().next().value?.username || ''],
-              position: [this.dataSource.data.values().next().value?.position || ''],
-              salary: [this.dataSource.data.values().next().value?.salary || 0],
-              password: [this.dataSource.data.values().next().value?.password || ''],
-              id: [this.dataSource.data.values().next().value?.id || 0],
-              rating: [this.dataSource.data.values().next().value?.rating || 0],
-              isEdit: [false]
-            },
-            {
-              select: [true],
-              name: [this.dataSource.data.values().next().value?.name || ''],
-              username: [this.dataSource.data.values().next().value?.username || ''],
-              position: [this.dataSource.data.values().next().value?.position || ''],
-              salary: [this.dataSource.data.values().next().value?.salary || 0],
-              password: [this.dataSource.data.values().next().value?.password || ''],
-              id: [this.dataSource.data.values().next().value?.id || 0],
-              rating: [this.dataSource.data.values().next().value?.rating || 0],
-              isEdit: [false]
-            },
-            {
-              select: [true],
-              name: [this.dataSource.data.values().next().value?.name || ''],
-              username: [this.dataSource.data.values().next().value?.username || ''],
-              position: [this.dataSource.data.values().next().value?.position || ''],
-              salary: [this.dataSource.data.values().next().value?.salary || 0],
-              password: [this.dataSource.data.values().next().value?.password || ''],
-              id: [this.dataSource.data.values().next().value?.id || 0],
-              rating: [this.dataSource.data.values().next().value?.rating || 0],
-              isEdit: [false]
-            },
-            {
-              select: [false],
-              name: [this.dataSource.data.values().next().value?.name || ''],
-              username: [this.dataSource.data.values().next().value?.username || ''],
-              position: [this.dataSource.data.values().next().value?.position || ''],
-              salary: [this.dataSource.data.values().next().value?.salary || 0],
-              password: [this.dataSource.data.values().next().value?.password || ''],
-              id: [this.dataSource.data.values().next().value?.id || 0],
-              rating: [this.dataSource.data.values().next().value?.rating || 0],
-              isEdit: [false]
-            }
-          ]) 
-        });
-        console.log("Employee Form Group:", this.employeeForm);
+  //sorting data for table
+  @ViewChild(MatSort)sort: MatSort = new MatSort;
 
-        this.employees.forEach(emp => this.addTableRow(emp as unknown as Employee));
-        console.log("Form Array after adding employees:", this.getRows().value);
- }
+  formModeEnum = FORM_MODE;
+  searchText!: Employee;
+  employees: Employee[] = [];
+  appFilter: AppFilterPipe = new AppFilterPipe;
+
+  event!: EventEmitter<Employee>;
+  //Lifecycle Event
+
+  constructor(private employeeService: EmployeeService, private router: Router)  {
+    this.refreshTable();
+  }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
 
+     this.dataSource.filterPredicate = (data: Employee, filter: string) => {
+      return data.name.toLowerCase().includes(filter);
+     }
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
+  refreshTable() {
+    this.employeeService.findAll().subscribe((data: Employee[]) => {
+      this.dataSource.data = data;
+      console.log("Fetched Employees Data:", this.dataSource.data);
+      this.dataSource.paginator = this.paginator; 
+    });
+  }
+  
+  searchEmployeeByText(){
+    this.employeeService.findBySearchText(this.searchText).subscribe((emp: Employee) => {
+       console.log(emp);
+       console.log(this.searchText);
+    }) 
+    // appFilter.transform(this.employees, "vi", "name");
+  }
+
+  //Event Handlers
 
   checkedOrUnchecked(row: Employee) {
     if (row && row.isEdit === true) {
@@ -193,136 +149,55 @@ export class EmployeesComponent {
     }
   }
 
-  checkboxLabel(row?: Employee): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`; 
-  }
-
-  toggleAllRows() {
-    if (this.isAllSelected()) {
-      this.selection.clear();
-    } else {
-        this.dataSource.data.forEach(row => this.selection.select(row));
-    }
-  }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length; // Assuming 'dataSource' is your MatTableDataSource
-    return numSelected === numRows;
-  }
-
-  getRows(): FormArray {
-        return this.employeeForm.get('rows') as FormArray;
-  }
-
-  addTableRow(data: Employee): void {
-        const selectedEmployee = this.selection.selected[0];
-        const rowFormGroup = this.fb.group({
-          select: [false],
-          name: [selectedEmployee ? selectedEmployee.name : '', Validators.required],
-          username: [selectedEmployee ? selectedEmployee.username : '', [Validators.required, Validators.email]],
-          position: [selectedEmployee ? selectedEmployee.position : '', Validators.required],
-          salary: [selectedEmployee ? selectedEmployee.salary : 0, [Validators.required, Validators.min(0)]],
-          password: [selectedEmployee ? selectedEmployee.password : '', Validators.required],
-          id: [selectedEmployee ? selectedEmployee.id : 0, [Validators.required, Validators.min(0)]],
-          rating: [selectedEmployee ? selectedEmployee.rating : 0, [Validators.required, Validators.min(0)]],
-          isEdit: [selectedEmployee ? selectedEmployee.isEdit : false]
-        });
-        this.getRows().push(rowFormGroup);
+  addRowButtonHandler(formMode:FORM_MODE){
+      if(formMode==FORM_MODE.Add){
+        this.router.navigateByUrl('/add-employee/');
+        console.log("Add Row");
       }
+  }
 
 
-      ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
-      }
-
-    addRow(mode:number){
-        if(mode==0){
-          this.router.navigateByUrl('/add-employee');
-          console.log("Add Row");
-        }
-        if(mode==1){
-          console.log("Edit Row");
-          console.log("Edit Row ID:", this.selection.selected[0].id);
-          //patch the form with selected row data
-              
-          if(this.selection.selected.length==0){
-            alert("Please select a row to edit");
-            this.selection.toggle(this.selection.selected[0]);
-            this.employeeForm.patchValue({
-              id: this.selection.selected[0]?.id || 0,
-              name: this.selection.selected[0]?.name || '',
-              position: this.selection.selected[0]?.position || '',
-              salary: this.selection.selected[0]?.salary || 0,
-              username: this.selection.selected[0]?.username || '',
-              password: this.selection.selected[0]?.password || '',
-              rating: this.selection.selected[0]?.rating || 0,
-              isEdit: this.selection.selected[0]?.isEdit || false
-            });
-            console.log("Patched Employee Form:", this.employeeForm.value);
-            alert("Row has been selected");
-            this.selection.changed.subscribe(s => { 
-              console.log("Selection changed:", s);
-            });
-            return;
-          }
-          if(this.selection.selected.length>1){
-            alert("Please select only one row to edit");
-            return;
-          }
-          if(this.selection.selected[0].id==undefined || this.selection.selected[0].id==0){
-            alert("Selected row has invalid ID");
-            return;
-          }
-          console.log("Navigating to Edit Employee component with employee ID:", this.selection.selected[0].id);
-          // this.$newEmployee = this.selection.selected[0];
-          // console.log("Selected Employee for Edit:", this.$newEmployee);
-          // this.router.navigateByUrl('/edit-employee');
-        
-          return;
-        }
-    }
-
-    editRow(){
-          console.log("Edit an employee id from employee table:  ", this.selection.selected[0].id);
+  editRowButtonHandler(formMode:FORM_MODE){
+          if(formMode==FORM_MODE.Edit){
+          console.log("Edit an employee id from employee table:  ", this.selection.selected[0]);
           if(this.selection.selected.length>0){
-          console.log("Selected Employee for Edit:", this.selection.selected[0]);
-          this.employeeService.getEmployeeById(1)
-            .subscribe((employee: Employee |  undefined) => {
-              if (employee) {
-                console.log("Employee fetched by ID:", employee.id);
-                this.$newEmployee = employee;
-                console.log("Navigating to Edit Employee component with employee:", employee);
-                this.router.navigateByUrl('/edit-employee');
-              } else {
-                console.log("Employee not found.");
-              }
-            });
+            console.log("Selected Employee for Edit:", this.selection.selected[0]);
+            this.router.navigateByUrl('/add-employee/'+this.selection.selected[0].id);
           }
           else
             console.log("no selection");
-    }
+      }
+  }
 
-    deleteRow(){
+  deleteRowButtonHandler(formMode:FORM_MODE){
+     if(formMode==FORM_MODE.Delete) {
       if(this.selection.selected.length>0){
-      console.log("Deleted an employee id from employee table:  ", this.selection.selected[0].id);
-      this.employeeService.deleteEmployee(this.selection.selected[0].id);
+        console.log("Deleted an employee id from employee table:  ", this.selection.selected[0].id);
+
+        this.employeeService.delete(this.selection.selected[0].id).subscribe({
+          next: (response: Boolean) => {
+            console.log(response);
+            console.log("Employee registered successfully", response);
+            this.refreshTable();
+          },
+          error: (error: string) => {
+            console.log(error);
+            console.error("Error during registration", error);
+          }
+        })
+
       }
       else
         console.log("no selection")
     }
+  }
 
-    cancel(){
-        this.employees.forEach(emp => emp.isEdit = false);
-        console.log("Edit Cancelled");
-    }
+  applyFilter(event: Event){
+     const filterValue = (event.target as unknown as HTMLInputElement).value;
+     this.dataSource.filter = filterValue.trim().toLowerCase();
+     console.log(this.dataSource.filter);
+     console.log(filterValue);
+  }
 
-    onSubmit(){
-        this.employees.push(this.$newEmployee);
-        console.log("New Employee Added:", this.$newEmployee);
-    }
 }
       
